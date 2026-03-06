f6c0fca0394dc16d5b38499a90db67c25f30f465
kish9013 <85638032+kish9013@users.noreply.github.com>
2026-03-06 13:49:26 +0530

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:19:16.803Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T08:19:25.083Z
+Commit: Patch API endpoint error handling
