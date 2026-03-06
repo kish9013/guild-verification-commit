997a8cf0d8fa284686942ab0759c6e6215b7db3c
kish9013 <85638032+kish9013@users.noreply.github.com>
2026-03-06 13:42:34 +0530

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:12:31.370Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T08:12:33.615Z
+Commit: Resolve null pointer exception in user service
