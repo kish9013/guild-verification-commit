82431b04520ef824d4be0e0fae5ad11128389418
kish9013 <85638032+kish9013@users.noreply.github.com>
2026-03-06 13:44:15 +0530

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:14:06.868Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T08:14:14.252Z
+Commit: Fix broken links in documentation
