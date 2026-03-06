82d3c0e71a483823fc0f3cb47657a9ad5f4ab2aa
kish9013 <85638032+kish9013@users.noreply.github.com>
2026-03-06 13:46:20 +0530

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:15:47.952Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T08:16:19.133Z
+Commit: Patch API endpoint error handling
