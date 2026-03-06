2b321c967802b26ab646e621c39bca39b7390996
kish9013 <85638032+kish9013@users.noreply.github.com>
2026-03-06 13:46:49 +0530

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:16:19.133Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T08:16:47.951Z
+Commit: Fix memory leak in event handlers
