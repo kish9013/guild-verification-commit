5871032c6e1627e82b78e257e03f2651542ee3e7
kish9013 <85638032+kish9013@users.noreply.github.com>
2026-03-06 13:45:22 +0530

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:15:09.160Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T08:15:21.236Z
+Commit: Resolve timezone issues in date handling
